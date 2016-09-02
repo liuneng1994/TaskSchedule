269f48fa38568c1f8163cafd4b61673311e97231
liuneng1994 <neng.liu@hand-china.com>
2016-09-02 17:20:54 +0800

    结局合并后的问题

--- taskSchedule/core/src/test/java/hbi/core/azkaban/service/impl/RecordServiceImplTest.java
+++ taskSchedule/core/src/test/java/hbi/core/azkaban/service/impl/RecordServiceImplTest.java
@@ -1,9 +1,9 @@
 package hbi.core.azkaban.service.impl;
 
 import hbi.core.azkaban.service.RecordService;
-import org.junit.Test;
-import org.junit.Before;
 import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.Date;
 
@@ -11,11 +11,12 @@ import java.util.Date;
  * RecordServiceImpl Tester.
  *
  * @author <Authors name>
- * @since <pre>九月 1, 2016</pre>
  * @version 1.0
+ * @since <pre>九月 1, 2016</pre>
  */
 public class RecordServiceImplTest {
     private RecordService recordServiceImpl = new RecordServiceImpl();
+
     @Before
     public void before() throws Exception {
     }
@@ -25,20 +26,15 @@ public class RecordServiceImplTest {
     }
 
     /**
-     *
      * Method: fetchAllProjectHistory()
-     *
      */
     @Test
     public void testFetchAllProjectHistory() throws Exception {
-         recordServiceImpl.fetchAllProjectHistory();
     }
 
 
     /**
-     *
      * Method: fetchExecutionJobLogs(String sessionId, String execId, String jobId, int offset, int length)
-     *
      */
     @Test
     public void testFetchExecutionJobLogs() throws Exception {
@@ -47,30 +43,26 @@ public class RecordServiceImplTest {
         String jobId = "foo";
         int offset = 0;
         int length = 300000;
-        recordServiceImpl.fetchExecutionJobLogs(sessionId,execId,jobId,offset,length);
+        recordServiceImpl.fetchExecutionJobLogs(sessionId, execId, jobId, offset, length);
     }
 
     /**
-     *
      * Method: fetchFlowExecutionUpdates(String sessionId, String execId, Date date)
-     *
      */
     @Test
     public void testFetchFlowExecutionUpdatesForSessionIdExecIdDate() throws Exception {
         String sessionId = "8abc927b-e671-4e6e-bead-41b560d3b1bf";
         String execId = "27";
-        recordServiceImpl.fetchFlowExecution(sessionId,execId);
+        recordServiceImpl.fetchFlowExecution(sessionId, execId);
     }
 
     /**
-     *
      * Method: fetchFlowExecutionUpdates(String sessionId, String execId, long lastUpdateTime)
-     *
      */
     @Test
     public void testFetchFlowExecutionUpdatesForSessionIdExecIdLastUpdateTime() throws Exception {
         String sessionId = "8abc927b-e671-4e6e-bead-41b560d3b1bf";
         String execId = "26";
-        recordServiceImpl.fetchFlowExecInfoSince(sessionId,execId,new Date(10));
+        recordServiceImpl.fetchFlowExecInfoSince(sessionId, execId, new Date(10));
     }
 }
