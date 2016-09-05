5a513961a83af422a41a68d497116a268d49fd19
Yangyazheng <2017948437@qq.com>
2016-09-05 15:55:03 +0800

    主题bean、mapper

---
+++ taskSchedule/core/src/main/java/hbi/core/azkaban/entity/theme/Theme.java
@@ -0,0 +1,53 @@
+package hbi.core.azkaban.entity.theme;
+
+/**
+ * Created by yyz on 2016/9/5.
+ * yazheng.yang@hand-china.com
+ */
+public class Theme {
+    private int themeId;
+    private String themeName;
+    private String description;
+    private String projectName;
+    private int projectVersion;
+
+    public int getThemeId() {
+        return themeId;
+    }
+
+    public void setThemeId(int themeId) {
+        this.themeId = themeId;
+    }
+
+    public String getThemeName() {
+        return themeName;
+    }
+
+    public void setThemeName(String themeName) {
+        this.themeName = themeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public int getProjectVersion() {
+        return projectVersion;
+    }
+
+    public void setProjectVersion(int projectVersion) {
+        this.projectVersion = projectVersion;
+    }
+}
